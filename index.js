7943f5ea77c2751f0e8b62a5b7115b6f88c26edf
Nitesh Kumar <niteshkumar240198@gmail.com>
2023-04-30 13:15:27 +0530

    calci

---
+++ index.js
@@ -0,0 +1,62 @@
+const crypto = require("crypto");
+
+const process = require("process");
+
+let operation = process.argv[2];
+
+let a = +process.argv[3];
+let b = +process.argv[4];
+
+
+switch (operation) {
+
+  case "add":
+    console.log(a + b);
+    break;
+
+  case "sub":
+    console.log(a - b);
+    break;
+
+  case "mult":
+    console.log(a * b);
+    break;
+
+  case "divide":
+    console.log(a / b);
+    break;
+
+  case "sin":
+    console.log(Math.sin(a));
+    break;
+
+  case "cos":
+    console.log(Math.cos(a));
+    break;
+
+  case "tan":
+    console.log(Math.tan(a));
+    break;
+
+  case "random":
+
+    if(a){
+
+      const data=crypto.randomBytes(a);
+      console.log(data.toString("binary"));
+
+    }
+    
+    else{
+      console.log("Provide length for random number generation.");
+    }
+    break;
+
+    default:
+    console.log("Invalid operation");
+
+}
+
+
+
+
